a279cc71aa53186292a6efa785dd755c547a6d84
Christopher Samoilov <christophersamoilov@gmail.com>
2024-04-22 15:27:59 +0700

    Edit contacts & preview

--- src/Data/Contacts.elm
+++ src/Data/Contacts.elm
@@ -2,11 +2,15 @@ module Data.Contacts exposing (links, myName)
 
 import Data.DesignExperience exposing (Link)
 
+
 links : List Link
 links =
-    [ { label = "telegram", url = "https://t.me/christophersamoilov" }
+    [ { label = "linkedin", url = "https://linkedin.com/in/christophersamoilov" }
+    , { label = "telegram", url = "https://t.me/christophersamoilov" }
     , { label = "email", url = "mailto:christophersamoilov@gmail.com" }
     ]
 
+
 myName : String
-myName = "Christopher Samoilov"
+myName =
+    "Christopher Samoilov"
